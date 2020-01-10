76f6f0609c94e5424a0d94be170244f00e489978
Ryan Zmuda <43967290+Yoyolick@users.noreply.github.com>
2020-01-09 23:33:21 -0500

    Added Program Files
    first upload, not even done yet

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBackup = new System.Windows.Forms.Button();
+            this.pathInput = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.selectedProfile = new System.Windows.Forms.ComboBox();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnProfileHelp = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(449, 42);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Boneworks Save Manager";
+            // 
+            // btnBackup
+            // 
+            this.btnBackup.Location = new System.Drawing.Point(212, 107);
+            this.btnBackup.Name = "btnBackup";
+            this.btnBackup.Size = new System.Drawing.Size(126, 21);
+            this.btnBackup.TabIndex = 1;
+            this.btnBackup.Text = "Backup Current Save";
+            this.btnBackup.UseVisualStyleBackColor = true;
+            // 
+            // pathInput
+            // 
+            this.pathInput.Location = new System.Drawing.Point(22, 68);
+            this.pathInput.Name = "pathInput";
+            this.pathInput.Size = new System.Drawing.Size(368, 20);
+            this.pathInput.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Path to save data:";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(396, 66);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 4;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(477, 66);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(22, 23);
+            this.btnHelp.TabIndex = 5;
+            this.btnHelp.Text = "?";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(19, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Save profile:";
+            // 
+            // selectedProfile
+            // 
+            this.selectedProfile.FormattingEnabled = true;
+            this.selectedProfile.Items.AddRange(new object[] {
+            "Personal Save",
+            "100% unlock save"});
+            this.selectedProfile.Location = new System.Drawing.Point(22, 107);
+            this.selectedProfile.Name = "selectedProfile";
+            this.selectedProfile.Size = new System.Drawing.Size(103, 21);
+            this.selectedProfile.TabIndex = 7;
+            this.selectedProfile.SelectedIndexChanged += new System.EventHandler(this.selectedProfile_SelectedIndexChanged);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(344, 107);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(127, 21);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete Selected Save";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(40, 152);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(449, 268);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnProfileHelp
+            // 
+            this.btnProfileHelp.Location = new System.Drawing.Point(477, 106);
+            this.btnProfileHelp.Name = "btnProfileHelp";
+            this.btnProfileHelp.Size = new System.Drawing.Size(22, 21);
+            this.btnProfileHelp.TabIndex = 10;
+            this.btnProfileHelp.Text = "?";
+            this.btnProfileHelp.UseVisualStyleBackColor = true;
+            this.btnProfileHelp.Click += new System.EventHandler(this.btnProfileHelp_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(131, 107);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 21);
+            this.btnLoad.TabIndex = 11;
+            this.btnLoad.Text = "Load Profile";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 450);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnProfileHelp);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.selectedProfile);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pathInput);
+            this.Controls.Add(this.btnBackup);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Boneworks Save Manager";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBackup;
+        private System.Windows.Forms.TextBox pathInput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox selectedProfile;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnProfileHelp;
+        private System.Windows.Forms.Button btnLoad;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //make form non resizable
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+
+            //set path box to be previous entered path
+            string text = System.IO.File.ReadAllText(@"C: \Users\swoos\source\repos\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\data\saved_path.txt");
+            pathInput.Text = text;
+
+            //turn buttons off because no save selected
+            if (selectedProfile.SelectedText == "")
+            {
+                btnDelete.Enabled = false;
+                btnBackup.Enabled = false;
+                btnLoad.Enabled = false;
+            }
+
+            //set application icon
+            this.Icon = new Icon("C:\\Users\\swoos\\source\\repos\\WindowsFormsApp1\\WindowsFormsApp1\\bin\\Debug\\data\\application_icon.ico");
+
+            //set splash image
+            pictureBox1.Image = Image.FromFile("C:\\Users\\swoos\\source\\repos\\WindowsFormsApp1\\WindowsFormsApp1\\bin\\Debug\\data\\splashimage.jpg");
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Copy the path to your game save data here, it should look something like:\nC:\\Users\\YOUR USER HERE\\AppData\\LocalLow\\Stress Level Zero\\BONEWORKS\\resources1.dat", "Help Window", MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string[] savedPath = {pathInput.Text};
+            System.IO.File.WriteAllLines(@"C:\Users\swoos\source\repos\WindowsFormsApp1\WindowsFormsApp1\bin\Debug\data\saved_path.txt", savedPath);
+        }
+
+        private void selectedProfile_SelectedIndexChanged(object sender, EventArgs e)
+        {
+                btnDelete.Enabled = true;
+                btnBackup.Enabled = true;
+                btnLoad.Enabled = true;
+        }
+
+        private void btnProfileHelp_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("These are preset profiles, one you should back up your playthrough on and the other is a 100% unlock save for sandbox and arena", "Help Window", MessageBoxButtons.OK, MessageBoxIcon.Question);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
