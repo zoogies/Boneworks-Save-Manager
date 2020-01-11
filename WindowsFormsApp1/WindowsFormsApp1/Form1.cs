e7b2ab9e404129c6048edc502f2c9b72e7ded4b4
Ryan Zmuda <ryanzmuda@gmail.com>
2020-01-10 23:00:20 -0500

    Started work on linking save data python scripts
    pogchamp

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -19,7 +20,6 @@ namespace WindowsFormsApp1
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
             //C:\\Program Files (x86)\\Yoyolick\\Boneworks Save Manager
             //path of install when using visual studio installer
 
@@ -45,6 +45,29 @@ namespace WindowsFormsApp1
             pictureBox1.Image = Image.FromFile("C:\\Program Files (x86)\\Yoyolick\\Boneworks Save Manager\\data\\splashimage.jpg");
 
             //Connects to python files for moving saves
+            var username = Environment.UserName;
+            var psi = new ProcessStartInfo();
+            psi.FileName = @"C:\\Users\\" + username + "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Python 3.8 (64-bit).exe";
+
+            var backup_current_to_100 = @"C:\\Program Files(x86)\\Yoyolick\\Boneworks Save Manager\\data\\backup_current_to_100.py";
+
+            //args here if ever needed i dont have foresight
+            //https://www.youtube.com/watch?v=g1VWGdHRkHs if youre reading this its only possible because of this video
+
+            //psi.UseShellExecute = false; //for normal
+            //psi.CreateNoWindow = true; //for normal
+            psi.UseShellExecute = true; //for debug
+            psi.CreateNoWindow = false; //for debug
+
+            psi.RedirectStandardError = true;
+            var errors = "";
+
+            using(var process = Process.Start(psi))
+            {
+                errors = process.StandardError.ReadToEnd();
+            }
+
+
 
         }
 
