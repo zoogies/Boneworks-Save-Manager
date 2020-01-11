31076939b0c3e037d474b0d7ed28bf000ca08dbc
Ryan Zmuda <ryanzmuda@gmail.com>
2020-01-11 10:36:18 -0500

    updated for shutil
    not sure how im going to do this still

--- WindowsFormsApp1/WindowsFormsApp1/bin/Debug/data/backup_current_to_100.py
+++ WindowsFormsApp1/WindowsFormsApp1/bin/Debug/data/backup_current_to_100.py
@@ -1,4 +1,23 @@
+#download current save to 100 profile
+
+#imports
 import os
 import shutil
+import pathlib
 
-shutil.move()
+#open users boneworks path
+savedatapathfile = open("saved_path.txt","r")
+savedatapath = savedatapathfile.read()
+savedatapathfile.close()
+
+100savepath = "C:\Program Files (x86)\Yoyolick\Boneworks Save Manager\data\100_save"
+errors = ""
+results = ""
+
+if not 100savepath.exists():
+    errors = "File path to save profile not found, did you install it in the right place?"
+
+shutil.move(savedatapath, 100savepath)
+#   print("an error has occured, check your save data path")
+
+#code by ryan zmuda 2020
